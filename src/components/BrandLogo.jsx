28d881a507d3b05e1f157e4ed9e410a31daae904
diya-wellorgs <diyag.wellorgs@gmail.com>
2025-10-30 20:10:46 +0530

    added logos

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,10 +1,20 @@
-export default function BrandLogo({ logo, alt }) {
+const heights = {
+  1: 'invert h-16',
+  3: 'h-16',
+  5: 'h-28',
+  6: 'h-16',
+  7: 'h-28',
+  13: 'h-18',
+  14: 'h-16',
+};
+
+export default function BrandLogo({ logo, id }) {
   return (
-    <div className="shrink-0  duration-600">
+    <div className="shrink-0">
       <img
         src={logo}
-        alt={alt}
-        className="h-40   hover:grayscale-0 transition-all duration-600"
+        alt="img"
+        className={`${heights[id] ?? 'h-35'} w-auto hover:grayscale-0`}
       />
     </div>
   );
--- src/components/TrustedBrands.jsx
+++ src/components/TrustedBrands.jsx
@@ -1,22 +1,74 @@
 import { motion } from 'motion/react';
 
-import brandLogo1 from '../assets/brand-logo1.png';
-import brandLogo2 from '../assets/brand-logo2.avif';
-import brandLogo3 from '../assets/brand-logo3.png';
-import brandLogo4 from '../assets/brand-logo4.avif';
-import brandLogo5 from '../assets/brand-logo5.png';
-import brandLogo6 from '../assets/brand-logo6.avif';
-import brandLogo7 from '../assets/brand-logo7.png';
+// import brandLogo1 from '../assets/brand-logo1.png';
+// import brandLogo2 from '../assets/brand-logo2.avif';
+// import brandLogo3 from '../assets/brand-logo3.png';
+// import brandLogo4 from '../assets/brand-logo4.avif';
+// import brandLogo5 from '../assets/brand-logo5.png';
+// import brandLogo6 from '../assets/brand-logo6.avif';
+// import brandLogo7 from '../assets/brand-logo7.png';
+// import brandLogo8 from '../assets/brand-logo8.png';
+import logo1 from '../assets/logo1.png';
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
+import logo4 from '../assets/logo4.png';
+import logo5 from '../assets/logo5.png';
+import logo6 from '../assets/logo6.png';
+import logo7 from '../assets/logo7.png';
+import logo8 from '../assets/logo8.png';
+import logo9 from '../assets/logo9.png';
+import logo10 from '../assets/logo10.png';
+import logo11 from '../assets/logo11.png';
+import logo12 from '../assets/logo12.png';
+import logo13 from '../assets/logo13.png';
+import logo14 from '../assets/logo14.png';
+import logo15 from '../assets/logo15.png';
 import BrandLogo from './BrandLogo';
 
 const brands = [
-  { id: 1, logo: brandLogo1, alt: 'Semaphore' },
-  { id: 2, logo: brandLogo2, alt: 'Better Medicare Alliance' },
-  { id: 3, logo: brandLogo3, alt: 'LinkedIn' },
-  { id: 4, logo: brandLogo4, alt: 'Synthesia' },
-  { id: 5, logo: brandLogo5, alt: 'Shaadi' },
-  { id: 6, logo: brandLogo6, alt: 'AmplifyAI' },
-  { id: 7, logo: brandLogo7, alt: 'EmployoAI' },
+  // { id: 1, logo: brandLogo1, alt: 'Semaphore' },
+  // { id: 2, logo: brandLogo2, alt: 'Better Medicare Alliance' },
+  // { id: 3, logo: brandLogo3, alt: 'LinkedIn' },
+  // { id: 4, logo: brandLogo4, alt: 'Synthesia' },
+  // { id: 5, logo: brandLogo5, alt: 'Shaadi' },
+  // { id: 6, logo: brandLogo6, alt: 'AmplifyAI' },
+  // { id: 7, logo: brandLogo7, alt: 'EmployoAI' },
+  // {
+  //   id: 8,
+  //   logo: brandLogo8,
+  //   alt: 'Wellorgs',
+  //   className: 'invert',
+  // },
+  { id: 1, logo: logo1 },
+  { id: 2, logo: logo2 },
+  { id: 3, logo: logo3 },
+  { id: 4, logo: logo4 },
+  { id: 5, logo: logo5 },
+  { id: 6, logo: logo6 },
+  { id: 7, logo: logo7 },
+  { id: 8, logo: logo8 },
+  { id: 9, logo: logo9 },
+  { id: 10, logo: logo10 },
+  { id: 11, logo: logo11 },
+  { id: 12, logo: logo12 },
+  { id: 13, logo: logo13 },
+  { id: 14, logo: logo14 },
+  { id: 15, logo: logo15 },
+  { id: 1, logo: logo1 },
+  { id: 2, logo: logo2 },
+  { id: 3, logo: logo3 },
+  { id: 4, logo: logo4 },
+  { id: 5, logo: logo5 },
+  { id: 6, logo: logo6 },
+  { id: 7, logo: logo7 },
+  { id: 8, logo: logo8 },
+  { id: 9, logo: logo9 },
+  { id: 10, logo: logo10 },
+  { id: 11, logo: logo11 },
+  { id: 12, logo: logo12 },
+  { id: 13, logo: logo13 },
+  { id: 14, logo: logo14 },
+  { id: 15, logo: logo15 },
 ];
 
 export default function TrustedBrands() {
@@ -30,26 +82,20 @@ export default function TrustedBrands() {
       {/* Logos Slider */}
       <div className="relative w-full max-w-3xl overflow-hidden z-10">
         <motion.div
-          className="flex gap-16 justify-center items-center"
-          animate={{ x: ['0%', '-100%'] }}
+          className="flex gap-16 w-fit items-center px-12"
+          animate={{ x: '-165%' }}
           transition={{
             repeat: Infinity,
             duration: 20,
+            delay: 1,
             ease: 'linear',
           }}
         >
           {brands.map((brand, index) => (
             <BrandLogo
+              id={brand.id}
               key={index}
               logo={brand.logo}
-              alt={brand.alt}
-            />
-          ))}
-          {brands.map((brand, index) => (
-            <BrandLogo
-              key={index}
-              logo={brand.logo}
-              alt={brand.alt}
             />
           ))}
         </motion.div>
