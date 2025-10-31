8c20f98f45b13b764c92f87bf725cb00493abf09
diya-wellorgs <diyag.wellorgs@gmail.com>
2025-10-31 16:13:37 +0530

    changes the brand logos

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,11 +1,14 @@
 const heights = {
-  1: 'invert h-16',
-  3: 'h-16',
-  5: 'h-28',
-  6: 'h-16',
-  7: 'h-28',
-  13: 'h-18',
-  14: 'h-16',
+  1: 'invert h-25',
+  2: 'h-45',
+  4: 'h-45',
+  5: 'h-35',
+  6: 'h-20',
+  7: 'h-35',
+  9: 'h-45',
+  13: 'h-35',
+  14: 'h-30',
+  15: 'h-50',
 };
 
 export default function BrandLogo({ logo, id }) {
@@ -14,7 +17,7 @@ export default function BrandLogo({ logo, id }) {
       <img
         src={logo}
         alt="img"
-        className={`${heights[id] ?? 'h-35'} w-auto hover:grayscale-0`}
+        className={`${heights[id] ?? 'h-40'} w-auto hover:grayscale-0`}
       />
     </div>
   );
