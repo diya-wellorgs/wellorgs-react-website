b1640bf23e86bf848a25d54fb2173f45aa89f431
diya-wellorgs <diyag.wellorgs@gmail.com>
2025-11-01 23:25:19 +0530

    added about us

---
+++ src/components/about/WhyUs.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { CheckMarkIcon } from '../../icons';
+import Chip from '../ui/Chip';
+import GradientText from '../ui/GradientText';
+
+const WhyUs = () => {
+  return (
+    <section className=" text-white py-24 flex flex-col items-center justify-center">
+      {/* Section Button */}
+      <div className="w-full flex flex-col items-center justify-center gap-7">
+        <Chip text=" Why us" />
+        {/* Gradient Heading & subheading*/}
+        <GradientText
+          title="What makes us stand out in the industry "
+          subheading="Discover how our innovative strategies, data-driven approach, and commitment to results set us apart from the competition"
+        />
+      </div>
+
+      {/* Cards Section */}
+      <div className="mt-16 grid grid-cols-1 md:grid-cols-2 gap-5 max-w-4xl w-full px-6">
+        {/* Manual Work Card */}
+        <div className="border border-neutral-800 rounded-md p-8 shadow-inner">
+          <h3 className="text-xl font-semibold mb-6">Manual Work</h3>
+          <ul className="space-y-1 text-neutral-300">
+            {[
+              'Prone to Human Errors',
+              'Limited by Work Hours',
+              'High Labor Costs & Overhead',
+              'Slow & Time-Consuming Tasks',
+              'Disconnected & Repetitive Work',
+              'Inconsistent & Dependent on Workforce',
+            ].map((item, i) => (
+              <li
+                key={i}
+                className="flex items-start gap-3"
+              >
+                <span className="text-gray-300 text-lg">✕</span>
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Our Solution Card */}
+        <div className="footer-bg-gradient border border-neutral-800 rounded-md p-8 shadow-inner">
+          <h3 className="text-xl font-semibold mb-6">Our Solution</h3>
+          <ul className="space-y-3 text-neutral-300">
+            {[
+              'Smart, AI-Driven Decisions',
+              '24/7 Automated Workflows',
+              'Scalable & Cost-Effective',
+              'Instant Data Processing',
+              'Seamless System Integration',
+              'Consistent & Reliable Output',
+            ].map((item, i) => (
+              <li
+                key={i}
+                className="flex items-start gap-3"
+              >
+                <CheckMarkIcon />
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyUs;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,128 @@
+import {
+  BsFillBarChartLineFill,
+  BsFillLightningChargeFill,
+} from 'react-icons/bs';
+import { IoTime } from 'react-icons/io5';
+
+import WhyUs from '../components/about/WhyUs';
+import FaqSection from '../components/faq/FaqSection';
+import TrustedBrands from '../components/TrustedBrands';
+import Chip from '../components/ui/Chip';
+import GradientText from '../components/ui/GradientText';
+
+const WhoWeAreData = [
+  {
+    icon: <BsFillLightningChargeFill className="w-6 h-6 text-white" />,
+    title: '95% Faster',
+    description:
+      'Clients see improved efficiency within the first three months of usage.',
+  },
+
+  {
+    icon: <IoTime className="w-6 h-6 text-white" />,
+    title: '1M+ Hours',
+    description:
+      'Reducing manual work and boosting productivity through automation.',
+  },
+
+  {
+    icon: <BsFillBarChartLineFill className="w-6 h-6 text-white" />,
+    title: '1.5K Clients',
+    description:
+      'AI adapts to your business needs, allowing you to scale efficiently without increasing workload or costs.',
+  },
+];
+
+const valuesData = [
+  {
+    icon: <BsFillLightningChargeFill className="w-6 h-6 text-white" />,
+    title: '95% Faster',
+    description:
+      'Clients see improved efficiency within the first three months of usage.',
+  },
+
+  {
+    icon: <IoTime className="w-6 h-6 text-white" />,
+    title: '1M+ Hours',
+    description:
+      'Reducing manual work and boosting productivity through automation.',
+  },
+
+  {
+    icon: <BsFillBarChartLineFill className="w-6 h-6 text-white" />,
+    title: '1.5K Clients',
+    description:
+      'AI adapts to your business needs, allowing you to scale efficiently without increasing workload or costs.',
+  },
+];
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="min-h-screen text-white flex flex-col items-center justify-center px-4 py-16 pt-30">
+      <div className="w-full flex flex-col items-center justify-center gap-7">
+        <Chip text=" About Us" />
+        {/* Gradient Heading & subheading*/}
+        <GradientText
+          title="Helping Businesses Grow"
+          subheading="Wellorgs helps businesses streamline operations and grow faster with AI-powered solutions."
+        />
+        <TrustedBrands showParticleBackground={false} />
+      </div>
+      <div className="max-w-6xl w-full flex flex-col items-center justify-center gap-15 px-6 text-center mx-auto ">
+        <div className="w-full flex flex-col items-center justify-center gap-7">
+          <Chip text=" Who We Are" />
+          {/* Gradient Heading & subheading*/}
+          <GradientText
+            title="Who We Are "
+            subheading="Wellorgs is a team of innovators dedicated to making AI automation simple and effective. We help businesses streamline workflows, boost efficiency, and scale with smart, AI-driven solutions."
+          />
+        </div>
+        {/* Who We Are Cards */}
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+          {WhoWeAreData.map((data, index) => (
+            <div
+              key={index}
+              className="bg-gradient-benefit border border-neutral-800 rounded-md p-6 text-left hover:shadow-[0_0_20px_rgba(255,255,255,0.05)] transition-all duration-300"
+            >
+              <div className="mb-3">{data.icon}</div>
+              <h3 className="text-xl font-semibold mb-2">{data.title}</h3>
+              <p className="text-gray-400 text-sm leading-relaxed">
+                {data.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* values */}
+      <div className="max-w-6xl w-full flex flex-col items-center justify-center gap-15 px-6 text-center mx-auto ">
+        <div className="w-full flex flex-col items-center justify-center gap-7">
+          <Chip text=" our values" />
+          {/* Gradient Heading & subheading*/}
+          <GradientText
+            title="The Values Behind Wellorgs AI "
+            subheading="Our values shape everything we do at Wellorgs. From innovation to integrity, we’re committed to building AI solutions that empower businesses and drive real impact."
+          />
+        </div>
+        {/* values Cards */}
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+          {valuesData.map((data, index) => (
+            <div
+              key={index}
+              className="bg-gradient-benefit border border-neutral-800 rounded-md p-6 text-left hover:shadow-[0_0_20px_rgba(255,255,255,0.05)] transition-all duration-300"
+            >
+              <div className="mb-3">{data.icon}</div>
+              <h3 className="text-xl font-semibold mb-2">{data.title}</h3>
+              <p className="text-gray-400 text-sm leading-relaxed">
+                {data.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <WhyUs />
+
+      <FaqSection />
+    </section>
   );
 }
 
